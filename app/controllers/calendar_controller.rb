a637fe9e37ed0cbea6ec33e2847421b18b3b6dac
Maciej Sypniewski <msypniewski511@gmail.com>
2017-03-28 23:57:51 +0200

    d

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,7 +1,7 @@
 class CalendarController < ApplicationController
-  before_action :set_time, only: [:show, :index]
+  before_action :set_time, only: :show
   def index
-    @moja = session[:month_number]
+    set_time
     @month = @time_now.month
     @year = @time_now.year
   end
@@ -17,14 +17,15 @@ class CalendarController < ApplicationController
   def set_time
   	time = Time.now
   	set_session
-    
-    if(session[:month_number] < 0) then
-      @time_now = time.months_ago(-(session[:month_number]))
-    elsif (session[:month_number] > 0) then
-      @time_now = time.months_since(session[:month_number])
+    if session[:month_number] then
+      if(session[:month_number] < 0) then
+        @time_now = time.months_ago(-(session[:month_number]))
+      elsif (session[:month_number] > 0) then
+        @time_now = time.months_since(session[:month_number])
       
-    else
-      @time_now = time
+      else
+        @time_now = time
+      end
     end
       	
 #  	if (params[:month] == "prev" && session[:month_number] > 0) then
@@ -43,8 +44,10 @@ class CalendarController < ApplicationController
   end
 
 
-  def set_session	
-
+  def set_session
+    if !session[:month_number] then
+      session[:month_number] = 0
+    end
   	if session[:month_number] then
   	  if(params[:month] == "prev") then
   	    session[:month_number] -= 1
