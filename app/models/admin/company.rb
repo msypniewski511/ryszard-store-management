7a53c321dde0c465e8067f36a813db6ad3ba0d9e
Maciej Sypniewski <msypniewski511@gmail.com>
2017-04-02 21:36:37 +0200

    try

--- app/models/admin/company.rb
+++ app/models/admin/company.rb
@@ -1,4 +1,4 @@
 class Admin::Company < ApplicationRecord
   validates :name, presence: true, uniqueness: {on: :create}
-  has_many :products, class_name: "Admin::Product"
+  has_many :products, class_name: "Admin::Product", dependent: :destroy
 end
